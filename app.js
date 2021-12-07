df02f950bd6cdca6b3b08598b48a56cf8beb23ad
rolfeshepshy <95461194+juan-iniguez@users.noreply.github.com>
2021-12-06 16:52:01 -0800

    Update app.js

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use(compression())
 app.use(expressEJSLayouts)
 app.set('view engine', 'ejs')
 app.set('layout', 'layout1')
-app.use('/assets' ,express.static('./assets'))
-app.use('/' ,express.static('./public'))
+app.use('/assets' ,express.static(__dirname + './assets'))
+app.use('/' ,express.static(__dirname + './public'))
 app.use(express.json({ limit: '50mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: true})); 
 
